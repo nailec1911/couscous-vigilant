ed07437f67438cb7353399fe3a20b74b6d807c96
celian <celian.lombardot@epitech.eu>
2024-12-12 18:51:07 +0100

    feat(analyzer): the fen parsing for the board is done, still need to do the turn,rock,half...

---
+++ analyzer/board_parsing.py
@@ -0,0 +1,79 @@
+""" Parsing of the fen boards
+"""
+from typing import List
+
+
+class Board:
+    """Class to parse and store a chess board from it's fen notation
+    """
+
+    def __init__(self, fen: str):
+        self.fen: str = fen
+        self.pieces_list = ["p", "r", "n", "b",
+                            "q", "k", "P", "R", "N", "B", "Q", "K"]
+
+        def create_grid() -> List[List[bool]]:
+            return [[False for _ in range(8)] for _ in range(8)]
+
+        self.boards: List[List[List[bool]]] = []
+        for piece in self.pieces_list:
+            setattr(self, piece, create_grid())
+            self.boards.append(getattr(self, piece))
+
+        self.__parse()
+
+    def __parse_infos(self, turn: str, rock: str, enpassant: str, halfmove: str, fullmove: str):
+        # TODO check that there are no errors and save them
+        pass
+
+    def __parse_board(self, board: str):
+        lines = board.split('/')
+        if len(lines) != 8:
+            raise ValueError("The board isn't the good size")
+        for i, line in enumerate(lines):
+            j = 0
+            for piece in line:
+                if j >= 8:
+                    raise ValueError(f"Line {i} is invalid")
+                if piece.isdigit():
+                    j += int(piece)
+                    continue
+                if not piece in self.pieces_list:
+                    raise ValueError(
+                        f"Unknown piece '{piece}' on the board at line {i}")
+                getattr(self, piece)[i][j] = True
+                j += 1
+
+    def __check_boards(self):
+        # TODO check that the number of pieces is ok and that there aren't two pieces on the same line
+        pass
+
+    def __parse(self):
+        splitted = self.fen.split(' ')
+        if len(splitted) != 6:
+            raise ValueError("Not enought information")
+        self.__parse_board(splitted[0])
+        self.__parse_infos(*splitted[1:])
+
+    def __repr__(self):
+        res = ""
+        for piece in self.pieces_list:
+            res += f"{piece}\n"
+            board = getattr(self, piece)
+            res += '\n'.join([' '.join([piece if p else '.' for p in line])
+                             for line in board])
+            res += '\n'
+        return res
+
+
+if __name__ == "__main__":
+    boards = [
+        "rnb1kbnr/pppp1ppp/8/4p3/6Pq/5P2/PPPPP2P/RNBQKBNR w KQkq - 1 3",
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        "rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR b KQkq d3 0 1",
+        "rnbqkbnr/pppp2pp/8/4pp1Q/3P4/4P3/PPP2PPP/RNB1KBNR b KQkq - 1 3",
+        "8/8/8/8/8/8/8/k1K5 w - - 0 1",
+    ]
+    a = Board(boards[0])
+    # print(a.boards)
+    print(a)
--- my_torch_analyzer.py
+++ my_torch_analyzer.py
@@ -4,46 +4,49 @@ import argparse
 from sys import stderr
 from analyzer.predict import predict
 from analyzer.train import train
+
+
 def parse_arguments():
-    parser = argparse.ArgumentParser(prog = "my_torch_analyzer",
-        description="A neural network analyzer for training or prediction using chessboard data."
+    parser = argparse.ArgumentParser(
+        prog="my_torch_analyzer",
+        description="A neural network analyzer for training or prediction using chessboard data.",
     )
 
     mode_group = parser.add_mutually_exclusive_group(required=True)
     mode_group.add_argument(
-        '--predict',
-        action='store_true',
-        help="Launch the neural network in prediction mode. Each chessboard in FILE" \
-            "must contain inputs to send to the neural network in FEN notation, and optionally an" \
-            "expected output."
+        "--predict",
+        action="store_true",
+        help="Launch the neural network in prediction mode. Each chessboard in FILE"
+        "must contain inputs to send to the neural network in FEN notation, and optionally an"
+        "expected output.",
     )
     mode_group.add_argument(
-        '--train',
-        action='store_true',
-        help="Launch the neural network in training mode. Each chessboard in FILE must" \
-            "contain inputs to send to the neural network in FEN notation and the expected output" \
-            "separated by space. If specified, the newly trained neural network will be saved in" \
-            "SAVEFILE. Otherwise, it will be saved in the original LOADFILE." \
+        "--train",
+        action="store_true",
+        help="Launch the neural network in training mode. Each chessboard in FILE must"
+        "contain inputs to send to the neural network in FEN notation and the expected output"
+        "separated by space. If specified, the newly trained neural network will be saved in"
+        "SAVEFILE. Otherwise, it will be saved in the original LOADFILE.",
     )
 
     parser.add_argument(
-        '--save',
-        metavar='SAVEFILE',
-        type=argparse.FileType('w'),
-        help="Save neural network into SAVEFILE. Only works in train mode."
+        "--save",
+        metavar="SAVEFILE",
+        type=argparse.FileType("w"),
+        help="Save neural network into SAVEFILE. Only works in train mode.",
     )
 
     parser.add_argument(
-        'LOADFILE',
-        metavar='LOADFILE',
-        type=argparse.FileType('r'),
-        help="File containing an artificial neural network"
+        "LOADFILE",
+        metavar="LOADFILE",
+        type=argparse.FileType("r"),
+        help="File containing an artificial neural network",
     )
     parser.add_argument(
-        'FILE',
-        metavar='FILE',
-        type=argparse.FileType('r'),
-        help="File containing chessboards."
+        "FILE",
+        metavar="FILE",
+        type=argparse.FileType("r"),
+        help="File containing chessboards.",
     )
 
     args = parser.parse_args()
@@ -68,6 +71,7 @@ def main_analyzer(args: argparse.Namespace) -> None:
     if args.train:
         train()
         return
+
 
 if __name__ == "__main__":
     try:
